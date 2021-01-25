0da89963bbbd020e8a76084909f1bed19d86925d
aniledev <elinamcgill@gmail.com>
2021-01-25 14:14:03 -0500

    FEAT: add the JSX for LearnMoreButton component

--- src/components/LearnMoreButton.js
+++ src/components/LearnMoreButton.js
@@ -4,14 +4,13 @@ import { Link } from "react-router-dom";
 export default class LearnMoreButton extends Component {
   render() {
     return (
-      <div class="container-md my-3">
-        <a
-          class="feature-button btn btn-outline-dark btn-md"
-          href="/features.html"
-          role="button"
+      <div className="container-md my-3">
+        <Link
+          className="feature-button btn btn-outline-dark btn-md"
+          to="/features"
         >
           Learn More
-        </a>
+        </Link>
       </div>
     );
   }
