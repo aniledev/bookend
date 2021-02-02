f599c781c387167a485884ad33a5ea62d0f7d291
aniledev <elinamcgill@gmail.com>
2021-02-01 19:56:47 -0500

    REFACTOR: add LogInTitle component to LogInForm component

---
+++ src/components/LogInTitle.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class LogInTitle extends Component {
+  render() {
+    return (
+      <div class="container-md log-in-title text-center py-2">
+        <h1>We knew you couldn't put us down either. Welcome back.</h1>
+      </div>
+    );
+  }
+}
