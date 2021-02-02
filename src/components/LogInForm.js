313289539b9dcb91564682113ef4023542bbbc7a
aniledev <elinamcgill@gmail.com>
2021-02-01 20:06:48 -0500

    REFACTOR: create a form component for the inputs in LogIn component

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class LogInForm extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div class="form container-md log-in-form">
+        <form class="log-in">
+          <div class="form-group">
+            <label for="email" class="sr-only">
+              Email
+            </label>
+            <input
+              class="form-control"
+              pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+              id="email"
+              placeholder="Email"
+              min="1"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <label for="password" class="sr-only">
+              Password
+            </label>
+            <input
+              class="form-control"
+              type="password"
+              id="password"
+              placeholder="Password"
+              min="8"
+              max="16"
+              required
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -3,6 +3,8 @@ import NavBar from "../components/navbar/NavBar";
 import Footer from "../components/Footer";
 import LogInTitle from "../components/LogInTitle";
 import LogInButtonSmall from "../components/buttons/LogInButtonSmall";
+import LogInButton from "../components/buttons/LogInButton";
+import LogInForm from "../components/LogInForm";
 
 export default class LogIn extends Component {
   render() {
@@ -13,38 +15,8 @@ export default class LogIn extends Component {
         <main class="main">
           <section class="log-in-section pt-5">
             <LogInTitle />
-
-            <div class="form container-md log-in-form">
-              <form class="log-in">
-                <div class="form-group">
-                  <label for="email" class="sr-only">
-                    Email
-                  </label>
-                  <input
-                    class="form-control"
-                    pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
-                    id="email"
-                    placeholder="Email"
-                    min="1"
-                    required
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="password" class="sr-only">
-                    Password
-                  </label>
-                  <input
-                    class="form-control"
-                    type="password"
-                    id="password"
-                    placeholder="Password"
-                    min="8"
-                    max="16"
-                    required
-                  />
-                </div>
-              </form>
-            </div>
+<LogInForm/>
+            
             <LogInButtonSmall />
           </section>
         </main>
