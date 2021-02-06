16058a57afca1826eaab03030bb3b7b957aadd02
aniledev <elinamcgill@gmail.com>
2021-02-06 02:02:19 -0500

    FILES: update modules naming conventions

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,32 @@
 import "../styles/App.css";
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "../pages/Home.js";
-import SignUp from "../pages/SignUp.js";
-import LogIn from "../pages/LogIn.js";
-import Search from "../pages/Search.js";
-import Results from "../pages/Results.js";
-import Features from "../pages/Features.js";
-import About from "../pages/About";
-import Contact from "../pages/Contact.js";
-import Profile from "../pages/Profile.js";
-import NotFound from "../pages/NotFound.js";
+import HomeView from "../pages/HomeView.js";
+import SignUpView from "../pages/SignUpView.js";
+import LogInView from "../pages/LogInView.js";
+import SearchView from "../pages/SearchView.js";
+import ResultsView from "../pages/ResultsView.js";
+import FeaturesView from "../pages/FeaturesView.js";
+import AboutView from "../pages/AboutView.js";
+import ContactView from "../pages/ContactView.js";
+import ProfileView from "../pages/ProfileView.js";
+import NotFoundView from "../pages/NotFoundView.js";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
-          <Route exact path="/sign-up" component={SignUp} />
-          <Route exact path="/log-in" component={LogIn} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/results" component={Results} />
-          <Route exact path="/features" component={Features} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
+          <Route exact path="/sign-up" component={SignUpView} />
+          <Route exact path="/log-in" component={LogInView} />
+          <Route exact path="/search" component={SearchView} />
+          <Route exact path="/results" component={ResultsView} />
+          <Route exact path="/features" component={FeaturesView} />
+          <Route exact path="/about" component={AboutView} />
+          <Route exact path="/contact" component={ContactView} />
+          <Route exact path="/profile" component={ProfileView} />
+          <Route exact path="/" component={HomeView} />
+          <Route component={NotFoundView} />
         </Switch>
       </div>
     );
